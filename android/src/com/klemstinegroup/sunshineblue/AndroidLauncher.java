bbc7ae8d665185f9abbc62e2efc9c252b9363102
Paul Klemstine <paulklemstine@gmail.com>
2021-06-11 06:46:14 -0500

    catch  sunshine.blue links, added to google play store

--- android/src/com/klemstinegroup/sunshineblue/AndroidLauncher.java
+++ android/src/com/klemstinegroup/sunshineblue/AndroidLauncher.java
@@ -1,9 +1,13 @@
 package com.klemstinegroup.sunshineblue;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import android.util.Log;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.klemstinegroup.sunshineblue.engine.util.NativeJava;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
@@ -11,6 +15,16 @@ public class AndroidLauncher extends AndroidApplication {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		config.depth=8;
-		initialize(new SunshineBlue(), config);
+		Intent intent = getIntent();
+		String action = intent.getAction();
+		Uri data = intent.getData();
+		int l=data.toString().indexOf("?");
+		if (l>0) {
+			String cid = data.toString().substring(data.toString().indexOf('?')+1);
+			initialize(new SunshineBlue(new NativeJava(), cid), config);
+		}
+		else{
+			initialize(new SunshineBlue(),config);
+		}
 	}
 }
