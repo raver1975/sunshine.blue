66d8c5d07f047941fc1cb872ec42a262b96750b8
Paul Klemstine <paulklemstine@gmail.com>
2022-11-05 19:43:44 -0500

    fixed record transform bug

--- core/src/com/klemstinegroup/sunshineblue/engine/objects/ScreenObject.java
+++ core/src/com/klemstinegroup/sunshineblue/engine/objects/ScreenObject.java
@@ -1,7 +1,12 @@
 package com.klemstinegroup.sunshineblue.engine.objects;
 
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.JsonValue;
+import com.klemstinegroup.sunshineblue.SunshineBlue;
+import com.klemstinegroup.sunshineblue.engine.data.DrawData;
+import com.klemstinegroup.sunshineblue.engine.data.FontData;
 import com.klemstinegroup.sunshineblue.engine.data.ScreenData;
+import com.klemstinegroup.sunshineblue.engine.util.SerializeUtil;
 
 public class ScreenObject extends BaseObject {
     public ScreenData sd=new ScreenData();
@@ -24,5 +29,23 @@ public class ScreenObject extends BaseObject {
         sd.position.sub(posDelta);
         sd.rotation-=rotDelta;
         sd.scale-=scaleDelta;
+    }
+
+    @Override
+    public JsonValue serialize() {
+        JsonValue val = new JsonValue(JsonValue.ValueType.object);
+        val.addChild("screenData", SerializeUtil.serialize(sd));
+        val.addChild("class", new JsonValue(DrawObject.class.getName()));
+        val.addChild("UUID", new JsonValue(uuid));
+        return val;
+    }
+
+    public static void deserialize(JsonValue json) {
+        ScreenData sd1 = SerializeUtil.deserialize(json.get("screenData"), ScreenData.class);
+        ScreenObject ftemp=new ScreenObject();
+        ftemp.sd=sd1;
+        ftemp.uuid=json.getString("UUID", ftemp.uuid);
+        SunshineBlue.addUserObj(ftemp);
+//        }
     }
 }
